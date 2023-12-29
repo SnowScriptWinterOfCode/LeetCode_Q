7b076d6942f90833ebecaf32f5ed203994fbc2ea
Shubh Krishna <135266175+Shubh-Krishna@users.noreply.github.com>
2023-12-29 22:32:10 +0530

    Update Word_Search.cpp

--- Others/Word_Search/Word_Search.cpp
+++ Others/Word_Search/Word_Search.cpp
@@ -1,22 +1,17 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 bool solve(vector<vector<char>> &board, int i, int j, string &word, int ind, vector<vector<bool>> &visited)
 {
-    //                                     0      0                       0
-    // base case
     if (ind >= word.length())
-    { // word length = 3
+    { 
         return true;
     }
-    int rows = board.size(), cols = board[0].size(); // 3 4
+    int rows = board.size(), cols = board[0].size(); 
     if ((i < 0 || i >= rows || j < 0 || j >= cols) || (board[i][j] != word[ind]) || visited[i][j] == true)
     {
         return false;
     }
 
     visited[i][j] = true;
-    bool downAns = solve(board, i + 1, j, word, ind + 1, visited); //
+    bool downAns = solve(board, i + 1, j, word, ind + 1, visited);
     bool leftAns = solve(board, i, j - 1, word, ind + 1, visited);
     bool rightAns = solve(board, i, j + 1, word, ind + 1, visited);
     bool upAns = solve(board, i - 1, j, word, ind + 1, visited);
@@ -44,30 +39,3 @@ bool exist(vector<vector<char>> &board, string word)
     return false;
 }
 
-int main()
-{
-    int col, row;
-    cin >> col >> row;
-    vector<vector<char>> board(row, vector<char>(col));
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            cin >> board[i][j];
-        }
-    }
-
-    string word;
-    cin >> word;
-
-    if (exist(board, word))
-    {
-        cout << "True" << endl;
-    }
-    else
-    {
-        cout << "False" << endl;
-    }
-
-    return 0;
-}
