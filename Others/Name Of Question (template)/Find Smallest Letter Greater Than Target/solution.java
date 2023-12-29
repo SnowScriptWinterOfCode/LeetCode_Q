281953b0a7313ebc8e690ed8583196e635a00c3c
Namita <46557775+namita0210@users.noreply.github.com>
2023-12-29 19:43:33 +0000

    made changes acc to comments

--- Others/Name Of Question (template)/Find Smallest Letter Greater Than Target/solution.java
+++ Others/Name Of Question (template)/Find Smallest Letter Greater Than Target/solution.java
@@ -1,19 +1,10 @@
-public class solution{
 
-    public static char nextGreatestLetter(char[] letters, char target) {
-        for (int i = 0 ; i < letters.length ; i++){
-            if(target<letters[i]){
-                return letters[i];
-            }
+public char nextGreatestLetter(char[] letters, char target) {
+    for (int i = 0 ; i < letters.length ; i++){
+        if(target<letters[i]){
+            return letters[i];
         }
-        return letters[0];
     }
-
-    public static void main(String[]args){
-        char[] letters = {'c','f','j'};
-        char target = 'c';
-        char ans ;
-        ans =nextGreatestLetter(letters,target);
-        System.out.println(ans);
-    }
+    return letters[0];
 }
+
