3afca934bbd04e3265ec632d88586ef4040b24b3
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2023-12-31 00:31:28 +0530

    Create solution.java

---
+++ Longest Repeating Character Replacement/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int [] character_count = new int[26]; // to keep track of different alphabets
+        int n = s.length(); //to get length or size of string
+// basic concept is that aslong k is not zero we can count and accomodate other string as we can change them
+// when k becomes 0 we can no longer add different string to the window      
+        int start_window = 0; //opening the window
+        int max_len=0; //maximum length of substring
+        int max_count =0;// to see which character occurs most in string
+
+
+        for (int end_window=0;end_window<n;end_window++)
+        {
+           character_count[s.charAt(end_window)-'A']++;
+           int present_charcount=character_count[s.charAt(end_window)-'A'];
+           max_count=Math.max(max_count,present_charcount);
+           while(end_window - start_window +1 -max_count > k) //window size-no of repeating letters
+           {
+              character_count[s.charAt(start_window)-'A']--; 
+              start_window++; 
+           }
+           max_len = Math.max(max_len,end_window-start_window +1);
+
+        }
+        return max_len;  
+    }
+}
