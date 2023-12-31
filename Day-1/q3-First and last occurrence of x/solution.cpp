d03c52f42f408accbfd1536700cd7582860fef5b
Ankita karn <91003709+Karnankita04@users.noreply.github.com>
2023-12-31 10:30:39 +0000

    Solved: Day-1 q3: First and last occurrences of x #5

--- Day-1/q3-First and last occurrence of x/solution.cpp
+++ Day-1/q3-First and last occurrence of x/solution.cpp
@@ -19,3 +19,27 @@ vector<int> searchRange(vector<int>& nums, int target) {
         return ans;
 
 }
+
+// Another solution.cpp
+
+class Solution {
+public:
+vector<int> searchRange(vector<int>& nums, int target) {        
+vector<int> ans(2, -1);
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==target){
+                ans[0]=i;
+                break;
+            }
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==target)
+            ans[1]=i;
+        }
+
+        return ans;
+}
+};
+
