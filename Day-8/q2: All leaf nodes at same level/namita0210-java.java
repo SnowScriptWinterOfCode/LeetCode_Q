d1340636549cb7d16dd2435ae870f12db41ad7b3
Namita <46557775+namita0210@users.noreply.github.com>
2024-01-08 03:21:18 +0000

    re format Q2 day 8 in java

--- Day-8/q2: All leaf nodes at same level/namita0210-java.java
+++ Day-8/q2: All leaf nodes at same level/namita0210-java.java
@@ -1,51 +1,53 @@
-class Node {
-    int data;
-    Node left, right;
+public class namita0210-java{
 
-    Node(int item) {
-        data = item;
-        left = right = null;
-    }
-}
 
-class BinaryTree {
-    Node root;
+    class Node {
+        int data;
+        Node left, right;
 
-    boolean checkSameLevelLeaves(Node root) {
-        if (root == null)
-            return true;
-
-        // Queue for level order traversal
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-
-        int level = 0;
-        int leafLevel = 0;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            level++;
-
-            while (size-- > 0) {
-                Node temp = queue.poll();
-
-                if (temp.left == null && temp.right == null) {
-                    if (leafLevel == 0) {
-                        leafLevel = level;
-                    } else if (leafLevel != level) {
-                        return false;
-                    }
-                }
-
-                if (temp.left != null)
-                    queue.add(temp.left);
-
-                if (temp.right != null)
-                    queue.add(temp.right);
-            }
+        Node(int item) {
+            data = item;
+            left = right = null;
         }
-        return true;
     }
 
- 
+    class BinaryTree {
+        Node root;
+
+        boolean checkSameLevelLeaves(Node root) {
+            if (root == null)
+                return true;
+
+            // Queue for level order traversal
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+
+            int level = 0;
+            int leafLevel = 0;
+
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                level++;
+
+                while (size-- > 0) {
+                    Node temp = queue.poll();
+
+                    if (temp.left == null && temp.right == null) {
+                        if (leafLevel == 0) {
+                            leafLevel = level;
+                        } else if (leafLevel != level) {
+                            return false;
+                        }
+                    }
+
+                    if (temp.left != null)
+                        queue.add(temp.left);
+
+                    if (temp.right != null)
+                        queue.add(temp.right);
+                }
+            }
+            return true;
+        } 
+    }
 }
