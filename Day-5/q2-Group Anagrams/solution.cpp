4359cc5bfc6ee69976900dfe1b9803f43cdcf923
Divya <divyalamba910@gmail.com>
2024-01-15 16:13:28 +0530

    solution added

---
+++ Day-5/q2-Group Anagrams/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mp;
+        for(int i=0; i<strs.size(); i++){
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            mp[s].push_back(strs[i]);
+        }
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
