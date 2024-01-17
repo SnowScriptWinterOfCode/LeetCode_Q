63150fbb6e3546dfc7208489c5e3b4ea3b6b9b6e
Shubh Krishna <135266175+Shubh-Krishna@users.noreply.github.com>
2024-01-17 20:44:43 +0530

    Update solution.cpp

--- Day-19/q3-Letter Combinations of a Phone Number/solution.cpp
+++ Day-19/q3-Letter Combinations of a Phone Number/solution.cpp
@@ -1,1 +1,19 @@
 
+ 	vector<string> res;
+ 	string key[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+ public:
+ 	vector<string> letterCombinations(string digits) {
+ 		if(digits.empty()) return res_;
+ 		recur(digits, "", 0);
+ 		return res;
+ 	}
+ 	void recur(string s, string cur, int index){
+ 		if(index == s.length()) res_.emplace_back(cur);
+ 		else{
+ 			string letters = key[s[index] - '0'];
+ 			for(const char c: letters){
+ 				recur(s, cur + c, index + 1);
+ 			}
+ 		}
+ 	}
+
