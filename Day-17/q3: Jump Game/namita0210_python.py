e04a316f63ad4f8ade38f6b45649886447e98266
Namita <46557775+namita0210@users.noreply.github.com>
2024-01-20 05:33:49 +0000

    Day-17 q3: Jump Game #382 - Fixed Python code

--- Day-17/q3: Jump Game/namita0210_python.py
+++ Day-17/q3: Jump Game/namita0210_python.py
@@ -16,9 +16,4 @@ def can_jump(nums):
 
     return False
 
-# Example usage:
-nums1 = [2, 3, 1, 1, 4]
-print(can_jump(nums1))  # Output: True
 
-nums2 = [3, 2, 1, 0, 4]
-print(can_jump(nums2))  # Output: False
