f6df1c8e13e6d3cefc5ec269d7d0d78cb0d9d9f5
Avani Mathur <151984867+avanimathur@users.noreply.github.com>
2024-01-25 07:57:49 +0530

    Update Day9 q2

---
+++ Day 9/q2: First and Last Position of Element in Sorted Array/avanimathur--C.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    vector<int> searchRange(const vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        vector<int> result = {-1, -1};
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[0] = mid;
+                right = mid - 1; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        left = 0;
+        right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[1] = mid;
+                left = mid + 1;  
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+    
+};
