1eb34948906377cc42c56f882e5fcfc4f3c8937d
Shubh Krishna <135266175+Shubh-Krishna@users.noreply.github.com>
2024-01-27 18:09:19 +0530

    Update Shubh_Krishna_solution.cpp

--- Day-26/q3: Find first and last position of element in sorted array/Shubh_Krishna_solution.cpp
+++ Day-26/q3: Find first and last position of element in sorted array/Shubh_Krishna_solution.cpp
@@ -1,6 +1,51 @@
-vector<int> searchRange(vector<int>& nums, int target) {
-    int l = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-    int r = lower_bound(nums.begin(), nums.end(), target + 1) - nums.begin();
-    if (l == r) return {-1, -1};
-    return {l, r - 1};
-}
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        int start =0 ;
+        int n = 
+        int end = nums.size() -1;
+        int flag = 0;
+        while(end>=start){
+            int mid = (start+end)/2;
+
+            if(nums[mid]== target){
+                if(nums[mid-1]!= target || mid ==0){
+                    ans.push_back(mid);
+                    flag = 1;
+                    break;
+                }else{
+                    end = mid-1;
+                }
+            }else if(nums[mid]>target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        if(flag == 0){
+            ans.push_back(-1);
+        }
+        flag =0;
+        while(end>=start){
+            int mid = (start+end)/2;
+
+            if(nums[mid]== target){
+                if(nums[mid+1]!= target || mid ==(nums.size()-1)){
+                    ans.push_back(mid);
+                    return ans;
+                }else{
+                    start = mid+1;
+                }
+            }else if(nums[mid]>target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        if(flag == 0){
+            ans.push_back(-1);
+        }
+        return ans;
+    }
+};
