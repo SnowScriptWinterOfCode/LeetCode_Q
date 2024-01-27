0a43aa52a3195b9aa8e1cdb8b7a4ab256198da5d
Shubh Krishna <135266175+Shubh-Krishna@users.noreply.github.com>
2024-01-27 18:24:20 +0530

    Update Shubh_Krishna_solution.cpp

--- Day-23/q1: Minimum falling path sum/Shubh_Krishna_solution.cpp
+++ Day-23/q1: Minimum falling path sum/Shubh_Krishna_solution.cpp
@@ -1,22 +1,18 @@
- 
-    class Solution {
-public:
-    
-    int minFallingPathSum(vector<vector<int>>& matrix) {       
-        int n=matrix.size();
-        int m=matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
+   int minFallingPathSum(vector<vector<int>>& matrix) {       
+       int n=matrix.size();
+       int m=matrix[0].size();
+       vector<vector<int>>dp(n,vector<int>(m,0));
         
-        for(int j=0;j<m;j++)
-        {
-            dp[0][j]=matrix[0][j];    
-        }
+       for(int j=0;j<m;j++)
+       {
+           dp[0][j]=matrix[0][j];    
+       }
         
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                int ld=1e9,rd=1e9;
+       for(int i=1;i<n;i++)
+       {
+           for(int j=0;j<m;j++)
+           {
+               int ld=1e9,rd=1e9;
                 int up=matrix[i][j] + dp[i-1][j];
     
                 if(j-1>=0)
