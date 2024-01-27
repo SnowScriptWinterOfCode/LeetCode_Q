edbb1c3d5a507e03e60889802a4e749203037927
Akansha_chaurasia <125260131+Akansha77@users.noreply.github.com>
2024-01-27 07:51:26 +0000

    Day-26 q3: Find first and last position of element in sorted array #560 cpp code added by Akansha77

---
+++ Day-26/q3: Find first and last position of element in sorted array/Akansha--C.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+       if(binary_search(nums.begin(),nums.end(),target))
+        {
+            int idx=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+            int idx1=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+            return {idx,idx1-1};
+        }
+        return {-1,-1}; 
+    }
+};
