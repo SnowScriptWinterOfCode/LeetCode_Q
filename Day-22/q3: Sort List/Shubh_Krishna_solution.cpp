17857c44a8bbf33099bc408c6f6c922d4b4e2213
Shubh Krishna <135266175+Shubh-Krishna@users.noreply.github.com>
2024-01-27 18:38:14 +0530

    Update Shubh_Krishna_solution.cpp

--- Day-22/q3: Sort List/Shubh_Krishna_solution.cpp
+++ Day-22/q3: Sort List/Shubh_Krishna_solution.cpp
@@ -1,44 +1,37 @@
-
-class Solution {
-public:
-    /// Merge Function
-
-    ListNode *mergeList(ListNode *l1, ListNode *l2){
-        ListNode *prev=new ListNode(-1), *curr=prev;
-        while(l1 and l2){
-            if(l1->val<=l2->val){
-                curr->next=l1;
-                l1=l1->next;
-            }else{
-                curr->next=l2;
-                l2=l2->next;
-            }
-        curr=curr->next;
-        }
-        if(l1){
+ListNode *mergeList(ListNode *l1, ListNode *l2){
+    ListNode *prev=new ListNode(-1), *curr=prev;
+    while(l1 and l2){
+        if(l1->val<=l2->val){
             curr->next=l1;
             l1=l1->next;
-        }
-        if(l2){
+        }else{
             curr->next=l2;
             l2=l2->next;
         }
-    return prev->next;
+    curr=curr->next;
     }
-
-    /// Divide and Conquer Approach (Merge Sort)
-
-    ListNode* sortList(ListNode* head){
-        if(!head or !head->next) return head;
-        ListNode *slow=head, *fast=head, *temp=NULL;
-        while(fast and fast->next){
-            temp=slow;
-            slow=slow->next;
-            fast=fast->next->next;
+    if(l1){
+        curr->next=l1;
+        l1=l1->next;
+    }
+    if(l2){
+        curr->next=l2;
+        l2=l2->next;
         }
-        temp->next=NULL;
+return prev->next;
+}
 
-        ListNode *point1=sortList(head), *point2=sortList(slow);
-    return mergeList(point1,point2);        
+ListNode* sortList(ListNode* head){
+    if(!head or !head->next) return head;
+    ListNode *slow=head, *fast=head, *temp=NULL;
+    while(fast and fast->next){
+        temp=slow;
+        slow=slow->next;
+        fast=fast->next->next;
     }
+    temp->next=NULL;
+
+    ListNode *point1=sortList(head), *point2=sortList(slow);
+return mergeList(point1,point2);        
+}
 };
