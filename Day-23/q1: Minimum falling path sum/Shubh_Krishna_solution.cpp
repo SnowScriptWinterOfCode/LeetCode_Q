c362131ea2470a11e30d44aa57fc791bdca9dc90
Shubh Krishna <135266175+Shubh-Krishna@users.noreply.github.com>
2024-01-27 18:25:23 +0530

    Update Shubh_Krishna_solution.cpp

--- Day-23/q1: Minimum falling path sum/Shubh_Krishna_solution.cpp
+++ Day-23/q1: Minimum falling path sum/Shubh_Krishna_solution.cpp
@@ -13,22 +13,21 @@
            for(int j=0;j<m;j++)
            {
                int ld=1e9,rd=1e9;
-                int up=matrix[i][j] + dp[i-1][j];
+               int up=matrix[i][j] + dp[i-1][j];
     
-                if(j-1>=0)
-                    ld=matrix[i][j] + dp[i-1][j-1];
-                if(j+1<m)
-                    rd=matrix[i][j] + dp[i-1][j+1];
+               if(j-1>=0)
+                  ld=matrix[i][j] + dp[i-1][j-1];
+               if(j+1<m)
+                  rd=matrix[i][j] + dp[i-1][j+1];
                 
-                dp[i][j] = min(up,min(ld,rd));   
-            }
-        }
-        int mini=dp[n-1][0];
+               dp[i][j] = min(up,min(ld,rd));   
+         }
+      }
+      int mini=dp[n-1][0];
         
-        for(int j=1;j<m;j++)
-        {
-            mini=min(mini,dp[n-1][j]);
-        }
-        return mini; 
-    }
-};
+      for(int j=1;j<m;j++)
+      {
+         mini=min(mini,dp[n-1][j]);
+      }
+      return mini; 
+   }
