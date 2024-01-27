738aef069dd6d86db558de47b08c3eede7dc3c2d
Shubh Krishna <135266175+Shubh-Krishna@users.noreply.github.com>
2024-01-27 18:06:49 +0530

    Update Shubh_Krishna_solution.cpp

--- Day-26/q3: Find first and last position of element in sorted array/Shubh_Krishna_solution.cpp
+++ Day-26/q3: Find first and last position of element in sorted array/Shubh_Krishna_solution.cpp
@@ -1,5 +1,4 @@
-class Solution {
-public:
+
     vector<int> searchRange(vector<int>& nums, int target) {
         int l = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
         int r = lower_bound(nums.begin(), nums.end(), target + 1) - nums.begin();
